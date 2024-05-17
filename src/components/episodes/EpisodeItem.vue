6133eebe6cdeb16d764b1061f8957ec20b46d80c
Xen0Xys <red57101@gmail.com>
2024-05-17 21:16:51 +0200

    :sparkles: Add episode list style

--- src/components/episodes/EpisodeItem.vue
+++ src/components/episodes/EpisodeItem.vue
@@ -17,13 +17,36 @@ function onClick(){
 <template>
     <div class="episode-item" @click="onClick">
         <img :src="episode.thumbnail" alt="Episode thumbnail"/>
-        <h2>{{episode.title}}</h2>
-        <p>{{episode.number}}</p>
+        <div class="episode-details">
+            <h2>{{episode.title}}</h2>
+            <p>#{{episode.number}}</p>
+        </div>
     </div>
 </template>
 
 <style scoped>
 .episode-item:hover {
     background-color: #202020;
+}
+
+.episode-item{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    &>img{
+        aspect-ratio: 1/1;
+        width: 7.5rem;
+    }
+}
+
+.episode-details{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
 }
 </style>
--- src/components/episodes/EpisodeList.vue
+++ src/components/episodes/EpisodeList.vue
@@ -17,14 +17,72 @@ onMounted(async() => {
 </script>
 
 <template>
-    <h1>{{webtoonInfos.title}}</h1>
-    <img :src="webtoonInfos.backgroundBanner" alt="thumbnail"/>
-    <img :src="webtoonInfos.topBanner" alt="thumbnail"/>
-    <div v-for="episode in episodes" :key="episode.id">
-        <EpisodeItem :episode="episode"/>
+    <div class="episode-list" v-if="webtoonInfos.title">
+        <header class="episodes-header">
+            <h1 class="banner-title">{{webtoonInfos.title}}</h1>
+            <img :src="webtoonInfos.backgroundBanner" alt="thumbnail" class="banner-background"/>
+            <img :src="webtoonInfos.topBanner" alt="thumbnail" class="banner-top"/>
+        </header>
+        <div class="episodes-container">
+            <div v-for="episode in episodes" :key="episode.id">
+                <EpisodeItem :episode="episode"/>
+            </div>
+        </div>
     </div>
 </template>
 
-<style scoped>
+<style>
 
+.episode-list{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    background-color: #0F0F0F;
+}
+
+.episodes-header {
+    position: relative;
+    text-align: center;
+}
+
+.banner-title {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+    color: black;
+}
+
+.banner-background {
+    width: 100%;
+    height: auto;
+    display: block;
+}
+
+.banner-top {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    z-index: 0;
+}
+
+.episodes-container{
+    display: flex;
+    flex-direction: column;
+    align-self: center;
+    gap: 1rem;
+
+    width: 100%;
+    max-width: 50rem;
+
+    padding: 1rem;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    border-radius: 1rem;
+
+    background-color: #171717;
+}
 </style>
