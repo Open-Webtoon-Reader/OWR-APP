de49b94fdf644e047ec679ee4745bec0948df3fd
Xen0Xys <red57101@gmail.com>
2024-06-17 20:51:38 +0200

    :lipstick: Minor style fix

--- components/webtoons/webtoons/WebtoonItem.vue
+++ components/webtoons/webtoons/WebtoonItem.vue
@@ -27,9 +27,9 @@ onMounted(() => {
 </script>
 
 <template>
-    <div id="main" class="flex items-center cursor-pointer" @click="navigateTo(`/webtoon/${webtoon.id}`)">
-        <NuxtImg :src="sumToImageUrl(webtoon.thumbnail)" loading="lazy" class="aspect-square h-20 sm:h-24 md:h-28 xl:h-32"/>
-        <div id="infos" class="flex items-center w-full justify-between pl-2 h-20 sm:h-24 md:h-28 xl:h-32">
+    <div id="main" class="flex items-center cursor-pointer h-20 sm:h-24 md:h-28 xl:h-32" @click="navigateTo(`/webtoon/${webtoon.id}`)">
+        <NuxtImg :src="sumToImageUrl(webtoon.thumbnail)" loading="lazy" class="aspect-square h-full"/>
+        <div id="infos" class="flex items-center w-full justify-between pl-2 h-full">
             <div id="text-infos" class="flex flex-col justify-between gap-auto h-full py-1 sm:py-2">
                 <div class="flex flex-col gap-0 md:gap-1 lg:gap-2">
                     <div id="title" class="flex flex-row gap-2">
