638c96ddf7e3685f743748a87b10697577c5c611
Xen0Xys <red57101@gmail.com>
2024-06-19 21:10:01 +0200

    :sparkles: Add navigation between episodes

--- pages/episode/[id].vue
+++ pages/episode/[id].vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import * as apiurlMiddleware from "~/middleware/apiurl.middleware";
-import {getEpisodeImages} from "~/utils/api";
+import {getEpisodeImages, getEpisodeInfos} from "~/utils/api";
 import VisibilityObserver from "~/components/utils/VisibilityObserver.vue";
 
 definePageMeta({
@@ -13,13 +13,14 @@ definePageMeta({
 const id = useRoute().params.id as any as number;
 
 const episodeImages = ref<string[]>([]);
+const episodeInfos = ref<any>({});
 const maxIndex = ref<number>(10);
 
 function increaseMaxIndex(){
     maxIndex.value += 10;
 }
 
-onMounted(async() => {
+async function loadEpisodeImages(){
     const episodeState: any = useState(`episode-${id}`);
     if (episodeState.value && episodeState.value.length){
         episodeImages.value = episodeState.value;
@@ -28,16 +29,42 @@ onMounted(async() => {
     const response = await getEpisodeImages(id);
     episodeImages.value = response.data;
     episodeState.value = episodeImages.value;
+}
+
+async function loadEpisodeInfos(){
+    const episodeState: any = useState(`episode-infos-${id}`);
+    if (episodeState.value && episodeState.value.length){
+        episodeInfos.value = episodeState.value;
+        return;
+    }
+    const response = await getEpisodeInfos(id);
+    episodeInfos.value = response.data;
+    episodeState.value = episodeInfos.value;
+}
+
+onMounted(async() => {
+    loadEpisodeImages();
+    loadEpisodeInfos();
 });
 </script>
 
 <template>
-    <div class="flex flex-col">
-        <div v-for="image of episodeImages.slice(0, maxIndex)" :key="episodeImages.indexOf(image)" class="flex justify-center">
-            <NuxtImg v-if="episodeImages.indexOf(image) < maxIndex - 1" :src="sumToImageUrl(image)" alt="Episode Image" class="w-full md:w-2/3 lg:w-1/2 xl:w-1/3"/>
+    <div class="flex flex-col items-center w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
+        <h3 class="m-2">{{ episodeInfos.title }}</h3>
+        <div v-for="image of episodeImages.slice(0, maxIndex)" :key="episodeImages.indexOf(image)" class="w-full">
+            <NuxtImg v-if="episodeImages.indexOf(image) < maxIndex - 1" :src="sumToImageUrl(image)" alt="Episode Image" class="w-full"/>
             <VisibilityObserver v-else @on-display="increaseMaxIndex">
-                <NuxtImg :src="sumToImageUrl(image)" alt="Episode Image" class="w-full md:w-2/3 lg:w-1/2 xl:w-1/3"/>
+                <NuxtImg :src="sumToImageUrl(image)" alt="Episode Image" class=""/>
             </VisibilityObserver>
+        </div>
+        <Separator/>
+        <div id="footer" class="w-full flex flex-row justify-between py-4 px-8">
+            <Button variant="outline" :disabled="!episodeInfos.previousEpisodeId" @click="navigateTo(`/episode/${episodeInfos.previousEpisodeId}`, {replace: true})">
+                <Icon name="iconoir:arrow-left"/>
+            </Button>
+            <Button variant="outline" :disabled="!episodeInfos.nextEpisodeId" @click="navigateTo(`/episode/${episodeInfos.nextEpisodeId}`, {replace: true})">
+                <Icon name="iconoir:arrow-right"/>
+            </Button>
         </div>
     </div>
 </template>
