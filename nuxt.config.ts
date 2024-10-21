45476cf57bdb783aa94da8ca3f8322e4c0b93418
Xen0Xys <red57101@gmail.com>
2024-10-21 19:40:01 +0200

    :sparkles: Implement PWA support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,86 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
-  devtools: {enabled: true},
+    ssr: true,
+    devtools: {enabled: true},
 
-  modules: [
-      "@nuxtjs/tailwindcss",
-      "shadcn-nuxt",
-      "@nuxt/eslint",
-      "@nuxtjs/color-mode",
-      "nuxt-icon",
-      "@nuxt/image"
-  ],
+    modules: [
+        "@nuxtjs/tailwindcss",
+        "shadcn-nuxt",
+        "@nuxt/eslint",
+        "@nuxtjs/color-mode",
+        "nuxt-icon",
+        "@nuxt/image",
+        "@vite-pwa/nuxt"
+    ],
 
-  shadcn: {
-      prefix: "",
-      componentDir: "./components/ui"
-  },
+    shadcn: {
+        prefix: "",
+        componentDir: "./components/ui"
+    },
 
-  colorMode: {
-      preference: "system",
-      fallback: "dark",
-      classSuffix: ""
-  },
+    colorMode: {
+        preference: "system",
+        fallback: "dark",
+        classSuffix: ""
+    },
 
-  tailwindcss: {
-      cssPath: [
-          "~/assets/css/index.css",
-          {injectPosition: 0}
-      ],
-      configPath: "tailwind.config.ts",
-      exposeConfig: false,
-      viewer: true,
-  },
+    tailwindcss: {
+        cssPath: [
+            "~/assets/css/index.css",
+            {injectPosition: 0}
+        ],
+        configPath: "tailwind.config.ts",
+        exposeConfig: false,
+        viewer: true,
+    },
 
-  app: {
-      head: {
-          link: [{rel: "icon", type: "image/svg+xml", href: "/logo.svg"}]
-      }
-  },
+    app: {
+        head: {
+            link: [{rel: "icon", type: "image/svg+xml", href: "/logo.svg"}]
+        }
+    },
 
-  compatibilityDate: "2024-10-12"
+    pwa: {
+        manifest: {
+            name: "Open Webtoon Reader",
+            short_name: "OWR",
+            description: "An open-source webtoon reader",
+            theme_color: "#020817",
+            screenshots: [
+                {
+                    src: "/screenshot.png",
+                    type: "image/png",
+                    sizes: "1919x915",
+                    form_factor: "wide"
+                },
+                {
+                    src: "/screenshot.png",
+                    type: "image/png",
+                    sizes: "1919x915",
+                    form_factor: "narrow"
+                }
+            ],
+            icons: [
+                {
+                    src: "/logo-192.webp",
+                    sizes: "192x192",
+                    type: "image/webp"
+                },
+                {
+                    src: "/logo-512.webp",
+                    sizes: "512x512",
+                    type: "image/webp"
+                }
+            ],
+        },
+        workbox: {
+            navigateFallback: "/",
+        },
+        devOptions: {
+            enabled: true,
+            type: "module",
+        }
+    },
+
+    compatibilityDate: "2024-10-12"
 });
